2663f00baeffd8963adb2d1206bebbe06f05c614
InnocentWahome <innocentwahome@gmail.com>
2022-09-28 19:41:42 +0300

    finished template for manual controller

--- app/controllers/ussd/manual/manual.controller.js
+++ app/controllers/ussd/manual/manual.controller.js
@@ -1,57 +1,117 @@
 /* eslint-disable no-console */
 // eslint-disable-next-line no-unused-vars
 const { sms, ussd, menu } = require('../../../config/africastalking');
-const Model = require('../../../models/user.model');
-
-const dataToSave = {};
 
 module.exports = async function ManualController(req, res) {
   try {
     menu.state('entry-point-to-manual-controller', {
       run: () => {
-        menu.con('This is the manual controller'
-              + '\n1. Schedule a call back'
-              + '\n2. Read our FAQs'
-              + '\n3. About'
-              + '\n4. Subscriptions');
+        menu.con('Here you can get more information about the following services we offer'
+              + '\n1. Our project delivery system'
+              + '\n2. How it works'
+              + '\n3. Our mission'
+              + '\n4. Our vision'
+              + '\n5. Want to partner with us?');
       },
       // next object links to next state based on user input
       next: {
-        1: 'entry-point-to-register-controller',
-        2: 'entry-point-to-help-controller',
-        3: 'entry-point-to-about-controller',
-        4: 'entry-point-to-subscriptions-controller',
+        1: 'project-delivery-system',
+        2: 'how-it-works',
+        3: 'our-mission',
+        4: 'our-vision',
+        5: 'partner-with-us',
       },
     });
 
-    menu.state('end', {
-      run: async () => {
-        const tickets = menu.val;
-        dataToSave.tickets = tickets;
-        console.log(dataToSave);
-
-        // Save the data
-
-        const data = new Model({
-          name: dataToSave.name,
-          tickets: dataToSave.tickets,
-        });
-
-        const dataSaved = await data.save();
-        console.log(dataSaved);
-        const options = {
-          to: menu.args.phoneNumber,
-          message: `Hi ${dataToSave.name}, we've reserved ${dataToSave.tickets} tickets for you.`,
-        };
-        await sms.send(options);
-
-        menu.end(`Awesome! We have reserverd ${dataToSave.tickets} tickets for you mkubwa We have your tickets reserved. Sending a confirmation text shortly.`);
-      },
-    });
-
-    menu.state('quit', {
+    menu.state('project-delivery-system', {
       run: () => {
-        menu.end('Goodbye :)');
+        menu.con('Text test text'
+              + '\n0. Back'
+              + '\n1. Contact us with a question'
+              + '\n2. End');
+      },
+      // next object links to next state based on user input
+      next: {
+        0: 'entry-point-to-manual-controller',
+        1: 'entry-point-to-contact-controller',
+        2: 'quit-manual-controller',
+      },
+    });
+
+    menu.state('how-it-works', {
+      run: () => {
+        menu.con('Here at ........ we have a simple process for delivering projects to our clients'
+              + '\n1. Back'
+              + '\n2. Learn more'
+              + '\n3. End'
+              + '\n4. Our vision'
+              + '\n4. Want to partner with us?');
+      },
+      // next object links to next state based on user input
+      next: {
+        1: 'entry-point-to-manual-controller',
+        2: 'entry-point-to-contact-controller',
+        3: 'quit-manual-controller',
+      },
+    });
+
+    menu.state('our-mission', {
+      run: () => {
+        menu.con('Here you can get more information about the following services we offer'
+              + '\n1. Our project delivery system'
+              + '\n2. How it works'
+              + '\n3. Our mission'
+              + '\n4. Our vision'
+              + '\n4. Want to partner with us?');
+      },
+      // next object links to next state based on user input
+      next: {
+        1: 'project-delivery-system',
+        2: 'how-it-works',
+        3: 'our-mission',
+        4: 'partner-with-us',
+      },
+    });
+
+    menu.state('our-vision', {
+      run: () => {
+        menu.con('Here you can get more information about the following services we offer'
+              + '\n1. Our project delivery system'
+              + '\n2. How it works'
+              + '\n3. Our mission'
+              + '\n4. Our vision'
+              + '\n4. Want to partner with us?');
+      },
+      // next object links to next state based on user input
+      next: {
+        1: 'project-delivery-system',
+        2: 'how-it-works',
+        3: 'our-mission',
+        4: 'partner-with-us',
+      },
+    });
+
+    menu.state('partner-with-us', {
+      run: () => {
+        menu.con('Here you can get more information about the following services we offer'
+              + '\n1. Our project delivery system'
+              + '\n2. How it works'
+              + '\n3. Our mission'
+              + '\n4. Our vision'
+              + '\n4. Want to partner with us?');
+      },
+      // next object links to next state based on user input
+      next: {
+        1: 'project-delivery-system',
+        2: 'how-it-works',
+        3: 'our-mission',
+        4: 'partner-with-us',
+      },
+    });
+
+    menu.state('quit-manual-controller', {
+      run: () => {
+        menu.end('We hope you found the information useful. Please contact us if you have any questions.');
       },
     });
     console.log('result');
