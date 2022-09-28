3cd4be3a1bf0bd60122ab9f4342d2a420a1a7092
Joseph-Gicuguma <josephgicuguma@gmail.com>
2022-09-28 16:43:22 +0300

    menu

--- app/controllers/ussd/app.controller.js
+++ app/controllers/ussd/app.controller.js
@@ -10,65 +10,29 @@ const HelpController = require('./help/help.controller');
 const SubscriptionsController = require('./subscription/subscriptions.controller');
 const ManualController = require('./manual/manual.controller');
 
-const sessions = {};
 module.exports = async function AppController(req, res) {
   try {
-    const DefaultSession = menu.sessionConfig({
-      start(sessionId, callback) {
-        // initialize current session if it doesn't exist
-        // this is called by menu.run()
-        if (!(sessionId in sessions)) sessions[sessionId] = {};
-        callback();
-      },
-      end(sessionId, callback) {
-        // clear current session
-        // this is called by menu.end()
-        delete sessions[sessionId];
-        callback();
-      },
-      set(sessionId, key, value, callback) {
-        // store key-value pair in current session
-        sessions[sessionId][key] = value;
-        callback();
-      },
-      get(sessionId, key) {
-        return new Promise((resolve) => {
-          const value = sessions[sessionId][key];
-          resolve(value);
-        });
-      },
-    });
     menu.startState({
       run: () => {
         console.log('Starting app');
         // DefaultSessions.set('firstName', 'wahome innocent');
         const { sessionId } = menu.args;
         console.log('session id', sessionId);
-        // UssdMenu.UssdSessionConfig.start(sessionId, () => {
-        //   console.log('session started');
-        // });
-        // UssdMenu.UssdSessionConfig.set('firstName', 'wahome innocent');
-        // start the session
-        DefaultSession.start(sessionId, () => {
-          console.log('session started');
-        });
-        // set a variable called firstName to the state
-        DefaultSession.set(sessionId, 'firstName', 'wahome innocent', () => {
-          console.log('session set');
-        });
         // use menu.con() to send response without terminating session
         menu.con('Welcome! Ready to register for the Cool Devs Clean energy:'
               + '\n1. Get started'
-              + '\n2. Help!'
-              + '\n3. About'
-              + '\n4. Subscriptions');
+              + '\n2. Projects in your area'
+              + '\n3. Contact'
+              + '\n4. Manual/Guide'
+              + '\n5. Subscriptions');
       },
       // next object links to next state based on user input
       next: {
         1: 'entry-point-to-register-controller',
-        2: 'entry-point-to-help-controller',
-        3: 'entry-point-to-about-controller',
-        4: 'entry-point-to-subscriptions-controller',
+        2: 'entry-point-to-projects-controller',
+        3: 'entry-point-to-contact-controller',
+        4: 'entry-point-to-manual-controller',
+        5: 'entry-point-to-subscriptions-controller',
       },
     });
 
